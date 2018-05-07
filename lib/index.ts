cc250bffe120d25d8f762112afeb1816c69cba85
Simon Johansson <mail@simon-johansson.com>
2018-05-07 22:09:26 +0200

    Fix exports from index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,15 @@
 import SpotifyQuizQuestions from './SpotifyQuizQuestions';
+import {
+  SpotifyClientConstructorOptions,
+  QuestionOptions,
+  QuestionType,
+  Playlist,
+} from './interfaces';
+
 export default SpotifyQuizQuestions;
+export {
+  SpotifyClientConstructorOptions,
+  QuestionOptions,
+  QuestionType,
+  Playlist,
+};
